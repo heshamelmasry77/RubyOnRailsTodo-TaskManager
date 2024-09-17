3f382b515933a778233701ebb17086b66ffbb512
heshamelmasry77 <>
2024-09-17 17:26:33 +0200

    query params

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,8 @@ class MainController < ApplicationController
 
   def about
     @created_by = "Hesham El Masry"
+    @id = params[:id]
+    @page = params[:page]
     render("about")
     end
   def hello
